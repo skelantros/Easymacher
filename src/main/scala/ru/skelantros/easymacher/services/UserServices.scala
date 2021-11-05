c13f011addeb1463c7ce68cc43938dd63b70fc2b
skelantros <ad3fful@yandex.ru>
2021-11-06 01:09:29 +0300

    Update UserServices.scala
    1) Added updatePassword and updateInfo services (with related json case-classes).
    2) Added json encoders and decoders of UserLight in the companion object.

--- src/main/scala/ru/skelantros/easymacher/services/UserServices.scala
+++ src/main/scala/ru/skelantros/easymacher/services/UserServices.scala
@@ -1,15 +1,16 @@
 package ru.skelantros.easymacher.services
 
 import cats.Monad
-import org.http4s.{EntityEncoder, HttpRoutes}
+import cats.effect.Concurrent
+import org.http4s.{EntityDecoder, EntityEncoder, HttpRoutes}
 import org.http4s.dsl.Http4sDsl
 import org.http4s.circe._
 import io.circe.generic.auto._
 import cats.implicits._
-import ru.skelantros.easymacher.db.UserDb.{Select, SelectOffset}
+import ru.skelantros.easymacher.db.UserDb.{Select, SelectOffset, Update}
 import ru.skelantros.easymacher.entities.{Role, User}
 
-class UserServices[F[_] : Monad] {
+class UserServices[F[_] : Concurrent] {
   val dsl = new Http4sDsl[F] {}
   import dsl._
   import UserServices._
@@ -51,11 +52,44 @@ class UserServices[F[_] : Monad] {
     case GET -> Root / "user" :? UsernameParam(username) =>
       processDbDef(db.userByUsername(username))(userLight)
   }
+
+  def updatePassword(user: User)(implicit db: Update[F]): HttpRoutes[F] = HttpRoutes.of[F] {
+    case req @ POST -> Root / "update-password" =>
+      val body = req.as[UpdPassword]
+      body.flatMap { updPass =>
+        processDbDef(db.updatePassword(user.id, updPass.old, updPass.`new`))(identity)
+      }
+  }
+
+  def updateInfo(user: User)(implicit db: Update[F]): HttpRoutes[F] = HttpRoutes.of[F] {
+    case req @ POST -> Root / "update-info" =>
+      val body = req.as[UpdInfo]
+      body.flatMap { updInfo =>
+        val UpdInfo(email, username, firstName, lastName) = updInfo
+        processDbDef(db.updateInfo(user.id, firstName, lastName, username, email))(identity)
+      }
+  }
 }
 
 object UserServices {
   case class UserLight(id: Int, username: String, role: Role,
                        firstName: Option[String], lastName: Option[String])
+  object UserLight {
+    implicit def encoder[F[_]]: EntityEncoder[F, UserLight] = jsonEncoderOf
+    implicit def decoder[F[_] : Concurrent]: EntityDecoder[F, UserLight] = jsonOf
+  }
   def userLight(user: User) =
     UserLight(user.id, user.username, user.role, user.firstName, user.lastName)
+
+  case class UpdPassword(old: String, `new`: String)
+  object UpdPassword {
+    implicit def encoder[F[_]]: EntityEncoder[F, UpdPassword] = jsonEncoderOf
+    implicit def decoder[F[_] : Concurrent]: EntityDecoder[F, UpdPassword] = jsonOf
+  }
+
+  case class UpdInfo(email: Option[String], username: Option[String], firstName: Option[String], lastName: Option[String])
+  object UpdInfo {
+    implicit def encoder[F[_]]: EntityEncoder[F, UpdInfo] = jsonEncoderOf
+    implicit def decoder[F[_] : Concurrent]: EntityDecoder[F, UpdInfo] = jsonOf
+  }
 }
