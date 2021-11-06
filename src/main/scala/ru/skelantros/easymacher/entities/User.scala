4544eb2dc24f8146defc57fc03d95d591c54faf4
skelantros <ad3fful@yandex.ru>
2021-11-06 14:19:48 +0300

    Added User.activateToken field

--- src/main/scala/ru/skelantros/easymacher/entities/User.scala
+++ src/main/scala/ru/skelantros/easymacher/entities/User.scala
@@ -3,5 +3,6 @@ package ru.skelantros.easymacher.entities
 import ru.skelantros.easymacher.utils.Email
 import ru.skelantros.easymacher.utils.Types.OrThrowable
 
-case class User(id: Int, username: String, email: Email, password: String, role: Role, isActivated: Boolean,
+case class User(id: Int, username: String, email: Email, password: String, role: Role,
+                isActivated: Boolean, activateToken: String,
                 firstName: Option[String], lastName: Option[String])
--- src/test/scala/ru/skelantros/easymacher/services/UserServicesSpec.scala
+++ src/test/scala/ru/skelantros/easymacher/services/UserServicesSpec.scala
@@ -16,10 +16,10 @@ import ru.skelantros.easymacher.{CommonSpec, DbMocks}
 
 class UserServicesSpec extends AnyFlatSpec with CommonSpec {
   val usersSample = Seq(
-    User(1, "skelantros", Email("skelantros@easymacher.ru").get, "23052001", Role.Admin, true, Some("Alex"), Some("Egorowski")),
-    User(2, "adefful", Email("ad3fful@easymacher.ru").get, "1234", Role.User, true, Some("Alex"), None),
-    User(3, "g03th3", Email("g03th3@klassik.de").get, "5678", Role.User, true, None, None),
-    User(4, "damned", Email("damned@mail.ru").get, "xd", Role.User, false, None, None)
+    User(1, "skelantros", Email("skelantros@easymacher.ru").get, "23052001", Role.Admin, true, "001", Some("Alex"), Some("Egorowski")),
+    User(2, "adefful", Email("ad3fful@easymacher.ru").get, "1234", Role.User, true, "002", Some("Alex"), None),
+    User(3, "g03th3", Email("g03th3@klassik.de").get, "5678", Role.User, true, "003", None, None),
+    User(4, "damned", Email("damned@mail.ru").get, "xd", Role.User, false, "004", None, None)
   )
 
 
