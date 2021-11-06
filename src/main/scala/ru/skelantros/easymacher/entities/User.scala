3140f1b45b3817390dbf9fb5492b7201edaaadac
skelantros <ad3fful@yandex.ru>
2021-11-06 14:23:23 +0300

    Added default values of user name parameters

--- src/main/scala/ru/skelantros/easymacher/entities/User.scala
+++ src/main/scala/ru/skelantros/easymacher/entities/User.scala
@@ -5,4 +5,4 @@ import ru.skelantros.easymacher.utils.Types.OrThrowable
 
 case class User(id: Int, username: String, email: Email, password: String, role: Role,
                 isActivated: Boolean, activateToken: String,
-                firstName: Option[String], lastName: Option[String])
+                firstName: Option[String] = None, lastName: Option[String] = None)
