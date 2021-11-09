9dc7be3b73aec03e061d24b0b00309025356bb1c
skelantros <ad3fful@yandex.ru>
2021-11-09 13:49:29 +0300

    Moved User samples to CommonSpec

--- src/test/scala/ru/skelantros/easymacher/CommonSpec.scala
+++ src/test/scala/ru/skelantros/easymacher/CommonSpec.scala
@@ -5,6 +5,8 @@ import cats.effect.unsafe.IORuntime
 import org.http4s._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import ru.skelantros.easymacher.entities.{Role, User}
+import ru.skelantros.easymacher.utils.Email
 
 trait CommonSpec extends AnyFlatSpec with Matchers {
   implicit val runtime: IORuntime = IORuntime.global
@@ -17,4 +19,11 @@ trait CommonSpec extends AnyFlatSpec with Matchers {
       resp.body.compile.toVector.unsafeRunSync().isEmpty shouldBe true
     )(expected => resp.as[A].unsafeRunSync() shouldBe expected)
   }
+
+  val usersSample = Seq(
+    User(1, "skelantros", Email("skelantros@easymacher.ru").get, "23052001", Role.Admin, true, "001", Some("Alex"), Some("Egorowski")),
+    User(2, "adefful", Email("ad3fful@easymacher.ru").get, "1234", Role.User, true, "002", Some("Alex"), None),
+    User(3, "g03th3", Email("g03th3@klassik.de").get, "5678", Role.User, true, "003", None, None),
+    User(4, "damned", Email("damned@mail.ru").get, "xd", Role.User, false, "004", None, None)
+  )
 }
--- src/test/scala/ru/skelantros/easymacher/services/UserServicesSpec.scala
+++ src/test/scala/ru/skelantros/easymacher/services/UserServicesSpec.scala
@@ -15,14 +15,6 @@ import ru.skelantros.easymacher.utils.Email
 import ru.skelantros.easymacher.CommonSpec
 
 class UserServicesSpec extends AnyFlatSpec with CommonSpec {
-  val usersSample = Seq(
-    User(1, "skelantros", Email("skelantros@easymacher.ru").get, "23052001", Role.Admin, true, "001", Some("Alex"), Some("Egorowski")),
-    User(2, "adefful", Email("ad3fful@easymacher.ru").get, "1234", Role.User, true, "002", Some("Alex"), None),
-    User(3, "g03th3", Email("g03th3@klassik.de").get, "5678", Role.User, true, "003", None, None),
-    User(4, "damned", Email("damned@mail.ru").get, "xd", Role.User, false, "004", None, None)
-  )
-
-
   val skelantrosJson = Json.obj(
     "id" := 1,
     "username" := "skelantros",
