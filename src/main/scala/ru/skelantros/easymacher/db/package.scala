7b001089ca0f831844f54b3bb3c1df52678e6b73
skelantros <ad3fful@yandex.ru>
2021-11-30 19:38:35 +0300

    Renamed Error to DbError
    To not be confused with java.lang.Error type

--- src/main/scala/ru/skelantros/easymacher/db/package.scala
+++ src/main/scala/ru/skelantros/easymacher/db/package.scala
@@ -1,32 +1,32 @@
 package ru.skelantros.easymacher
 
 package object db {
-  type DbResult[+A] = Either[Error, A]
+  type DbResult[+A] = Either[DbError, A]
   type DbUnit = DbResult[Unit]
 
   object DbResult {
     def of[A](x: A): DbResult[A] = Right(x)
     val unit: DbUnit = Right(())
-    def mistake[A](msg: String): DbResult[A] = Left(Error.mistake(msg))
-    def thr[A](t: Throwable): DbResult[A] = Left(Error.thr(t))
+    def mistake[A](msg: String): DbResult[A] = Left(DbError.mistake(msg))
+    def thr[A](t: Throwable): DbResult[A] = Left(DbError.thr(t))
   }
 
   object DbUnit {
-    def mistake(msg: String): DbUnit = Left(Error.mistake(msg))
-    def thr(t: Throwable): DbUnit = Left(Error.thr(t))
+    def mistake(msg: String): DbUnit = Left(DbError.mistake(msg))
+    def thr(t: Throwable): DbUnit = Left(DbError.thr(t))
   }
 
-  sealed trait Error {
+  sealed trait DbError {
     def map[A](fMis: String => A, fThr: Throwable => A): A = this match {
       case Mistake(msg) => fMis(msg)
       case Thr(t) => fThr(t)
     }
   }
-  case class Mistake(msg: String) extends Error
-  case class Thr(t: Throwable) extends Error
+  case class Mistake(msg: String) extends DbError
+  case class Thr(t: Throwable) extends DbError
 
-  object Error {
-    def mistake(msg: String): Error = Mistake(msg)
-    def thr(t: Throwable): Error = Thr(t)
+  object DbError {
+    def mistake(msg: String): DbError = Mistake(msg)
+    def thr(t: Throwable): DbError = Thr(t)
   }
 }
