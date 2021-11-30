8a390fb613471e83a5c946f67a4135f758bcc4d3
skelantros <ad3fful@yandex.ru>
2021-12-01 00:49:15 +0300

    Added AuthLifter sequence functions

--- src/main/scala/ru/skelantros/easymacher/auth/AuthLifter.scala
+++ src/main/scala/ru/skelantros/easymacher/auth/AuthLifter.scala
@@ -1,10 +1,13 @@
 package ru.skelantros.easymacher.auth
 
+import cats.Monad
 import cats.data.Kleisli
+import cats.effect.kernel.Concurrent
+import cats.implicits._
 import org.http4s.HttpRoutes
+import org.http4s.implicits._
 import ru.skelantros.easymacher.entities.User
 
-// TODO добавить функции для преобразования нескольких сервисов разом
 object AuthLifter {
   def apply[F[_]](service: User => HttpRoutes[F]): UserRoutes[F] = Kleisli { authedReq =>
     val user = authedReq.context
@@ -12,7 +15,13 @@ object AuthLifter {
     service(user)(req)
   }
 
+  def apply[F[_] : Monad](services: (User => HttpRoutes[F])*): UserRoutes[F] =
+    services.map(AuthLifter(_)).reduce(_ <+> _)
+
   def apply[F[_]](service: HttpRoutes[F]): UserRoutes[F] = Kleisli { authedReq =>
     service(authedReq.req)
   }
+
+  def apply[F[_] : Monad](services: HttpRoutes[F]*): UserRoutes[F] =
+    services.map(AuthLifter(_)).reduce(_ <+> _)
 }
