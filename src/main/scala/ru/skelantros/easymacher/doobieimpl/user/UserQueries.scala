402a1bc078ed31bb34bc22d3dda47b2c05fd4508
skelantros <ad3fful@yandex.ru>
2021-11-30 18:46:31 +0300

    Changed UserQueries functions
    Now they present pure queries instead of ConnectionIOs. Pure queries can be used later in unit test analysis.

--- src/main/scala/ru/skelantros/easymacher/doobieimpl/user/UserQueries.scala
+++ src/main/scala/ru/skelantros/easymacher/doobieimpl/user/UserQueries.scala
@@ -14,34 +14,34 @@ object UserQueries {
         is_activated, activate_token, first_name, last_name)
   }
 
-  private implicit class UpdNote(upd: Update0) {
+  implicit class UpdNote(upd: Update0) {
     def note: ConnectionIO[Note] =
       upd.withUniqueGeneratedKeys[Note]("user_id", "email", "username", "activate_token", "is_activated", "passw", "first_name", "last_name", "is_admin")
   }
 
   private val selectAllFr = fr"select user_id, email, username, activate_token, is_activated, passw, first_name, last_name, is_admin from users"
 
-  def selectAll: ConnectionIO[List[Note]] =
-    sql"$selectAllFr".query[Note].to[List]
+  def selectAll =
+    sql"$selectAllFr".query[Note]
 
-  def selectByAdmin(isAdmin: Boolean): ConnectionIO[List[Note]] =
-    sql"$selectAllFr where is_admin = $isAdmin".query[Note].to[List]
+  def selectByAdmin(isAdmin: Boolean) =
+    sql"$selectAllFr where is_admin = $isAdmin".query[Note]
 
-  def selectById(id: Int): ConnectionIO[Option[Note]] =
-    sql"$selectAllFr where user_id = $id".query[Note].option
+  def selectById(id: Int) =
+    sql"$selectAllFr where user_id = $id".query[Note]
 
-  def selectByUsername(username: String): ConnectionIO[Option[Note]] =
-    sql"$selectAllFr where lower(username) = ${username.toLowerCase}".query[Note].option
+  def selectByUsername(username: String) =
+    sql"$selectAllFr where lower(username) = ${username.toLowerCase}".query[Note]
 
-  def selectByEmail(email: String): ConnectionIO[Option[Note]] =
-    sql"$selectAllFr where lower(email) = ${email.toLowerCase}".query[Note].option
+  def selectByEmail(email: String) =
+    sql"$selectAllFr where lower(email) = ${email.toLowerCase}".query[Note]
 
-  def selectByToken(token: String): ConnectionIO[Option[Note]] =
-    sql"$selectAllFr where activate_token = $token".query[Note].option
+  def selectByToken(token: String) =
+    sql"$selectAllFr where activate_token = $token".query[Note]
 
   def update(id: Int,
              email: Option[String], username: Option[String],
-             firstName: Option[String], lastName: Option[String]): ConnectionIO[Note] = {
+             firstName: Option[String], lastName: Option[String]): Update0 = {
     val fields1 =
       (fr"email", email) :: (fr"username", username) ::
         (fr"first_name", firstName) :: (fr"last_name", lastName) :: Nil
@@ -49,15 +49,15 @@ object UserQueries {
       case (fr, Some(value)) => fr"$fr = $value"
     }
     (fr"update users set " ++ frs.intercalate(fr",") ++ fr"where user_id = $id")
-      .update.note
+      .update
   }
 
 
-  def create(username: String, passw: String, email: String, isAdmin: Boolean, token: String): ConnectionIO[Note] =
-    sql"insert into users(username, passw, email, is_admin, activate_token, is_activated) values ($username, $passw, $email, $isAdmin, $token, false)"
-    .update.note
+  def create(username: String, passw: String, email: String, isAdmin: Boolean, token: String): Update0 =
+    sql"""insert into users(username, passw, email, is_admin, activate_token, is_activated)
+          values ($username, $passw, $email, $isAdmin, $token, false)"""
+    .update
 
-  def activate(token: String): ConnectionIO[Note] =
-    sql"update users set is_activated = true where activate_token = $token"
-    .update.note
+  def activate(token: String): Update0 =
+    sql"update users set is_activated = true where activate_token = $token".update
 }
