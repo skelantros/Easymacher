fab9ad12fd848456e5a390f52b7cd3c5735bdc5c
skelantros <ad3fful@yandex.ru>
2021-12-04 00:18:47 +0300

    Added Word groups DB implementation to CryptokeyMain

--- src/main/scala/ru/skelantros/easymacher/main/CryptokeyMain.scala
+++ src/main/scala/ru/skelantros/easymacher/main/CryptokeyMain.scala
@@ -4,13 +4,14 @@ import cats.effect.{ExitCode, IO, IOApp}
 import doobie.util.transactor.Transactor
 import org.http4s.HttpApp
 import ru.skelantros.easymacher.doobieimpl.user.UserDoobie
-import ru.skelantros.easymacher.services.{UserServices, WordServices}
+import ru.skelantros.easymacher.services.{UserServices, WordGroupServices, WordServices}
 import org.http4s.dsl.io._
 import org.http4s.implicits._
 import org.http4s._
 import cats.implicits._
 import org.http4s.blaze.server.BlazeServerBuilder
 import ru.skelantros.easymacher.auth.{AuthLifter, CryptokeyAuth, UserRoutes}
+import ru.skelantros.easymacher.doobieimpl.group.WordGroupDoobie
 import ru.skelantros.easymacher.doobieimpl.word.WordDoobie
 import ru.skelantros.easymacher.utils.TransactorImpl
 
@@ -20,9 +21,11 @@ object CryptokeyMain extends IOApp {
   implicit val transactor = TransactorImpl[IO]
   implicit val userDb = new UserDoobie[IO]
   implicit val wordsDb = new WordDoobie[IO]
+  implicit val wordGroupsDb = new WordGroupDoobie[IO]
 
   val userServices = new UserServices[IO]
   val wordServices = new WordServices[IO]
+  val groupServices = new WordGroupServices[IO]
 
   val auth = new CryptokeyAuth[IO]
 
@@ -37,7 +40,8 @@ object CryptokeyMain extends IOApp {
   val authIdServices: UserRoutes[IO] = AuthLifter(
     userServices.updateServices(_),
     wordServices.selectUserServices,
-    wordServices.addWord(_)
+    wordServices.addWord(_),
+    groupServices.allServices
   )
 
 
