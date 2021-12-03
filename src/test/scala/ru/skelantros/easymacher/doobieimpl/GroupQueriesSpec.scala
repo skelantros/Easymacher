3f5b4339c435f68c674f87cd5ee37e9a74f0d791
skelantros <ad3fful@yandex.ru>
2021-12-03 23:05:42 +0300

    Added Group related queries

---
+++ src/test/scala/ru/skelantros/easymacher/doobieimpl/GroupQueriesSpec.scala
@@ -0,0 +1,15 @@
+package ru.skelantros.easymacher.doobieimpl
+import group.GroupQueries._
+
+class GroupQueriesSpec extends AnalyzeSpec {
+  test("selectGroups") { check(selectGroups) }
+  test("selectGroupsByUserId") { check(selectGroupsByUserId(1)) }
+  test("insertGroup") { check(insertGroup(1, "name", true)) }
+  test("updateGroup NN") { check(updateGroup(1, None, None)) }
+  test("updateGroup SN") { check(updateGroup(1, Some("name"), None)) }
+  test("updateGroup NS") { check(updateGroup(1, None, Some(true))) }
+  test("updateGroup SS") { check(updateGroup(1, Some("name"), Some(true))) }
+  test("selectG2W") { check(selectG2W) }
+  test("selectG2WByGroupId") { check(selectG2WByGroupId(1)) }
+  test("insertG2W") { check(insertG2W(1, 1)) }
+}
