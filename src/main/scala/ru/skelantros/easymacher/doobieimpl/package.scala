7d4e599e05a823cff032adddcdd787121216f685
skelantros <ad3fful@yandex.ru>
2021-12-05 17:54:06 +0300

    Added doobie utils to deal with exceptions

--- src/main/scala/ru/skelantros/easymacher/doobieimpl/package.scala
+++ src/main/scala/ru/skelantros/easymacher/doobieimpl/package.scala
@@ -12,11 +12,37 @@ import doobie.implicits._
 import doobie.util.update.Update0
 
 package object doobieimpl {
+  object PsqlStates {
+    import doobie.postgres._
+    val uniqueViolation = sqlstate.class23.UNIQUE_VIOLATION
+    val foreignKeyViolation = sqlstate.class23.FOREIGN_KEY_VIOLATION
+  }
+
   def processSelect[F[_] : MonadCancelThrow, A, B](query: ConnectionIO[A])(f: A => B)(implicit xa: Transactor[F]): F[DbResult[B]] =
     for {
       fromDb <- query.transact(xa).attempt
       res = fromDb match {
         case Right(a) => DbResult.of(f(a))
+        case Left(t) => DbResult.thr(t)
+      }
+    } yield res
+
+  def processSelectEither[F[_] : MonadCancelThrow, A, B](query: ConnectionIO[Either[String, A]])(f: A => B)(implicit xa: Transactor[F]): F[DbResult[B]] =
+    for {
+      fromDb <- query.transact(xa).attempt
+      res = fromDb match {
+        case Right(Right(a)) => DbResult.of(f(a))
+        case Right(Left(msg)) => DbResult.mistake(msg)
+        case Left(t) => DbResult.thr(t)
+      }
+    } yield res
+
+  def processSelectUnitEither[F[_] : MonadCancelThrow, A](query: ConnectionIO[Either[String, A]])(implicit xa: Transactor[F]): F[DbUnit] =
+    for {
+      fromDb <- query.transact(xa).attempt
+      res = fromDb match {
+        case Right(Right(_)) => DbResult.unit
+        case Right(Left(msg)) => DbResult.mistake(msg)
         case Left(t) => DbResult.thr(t)
       }
     } yield res
