0266fda7feb10c52519c9912744553cf01d41746
skelantros <ad3fful@yandex.ru>
2021-12-09 01:25:20 +0300

    Added User.auth0Id field to existing User class
    The default value is preserved for reverse compatibility

--- src/main/scala/ru/skelantros/easymacher/entities/User.scala
+++ src/main/scala/ru/skelantros/easymacher/entities/User.scala
@@ -3,6 +3,8 @@ package ru.skelantros.easymacher.entities
 import ru.skelantros.easymacher.utils.Email
 import ru.skelantros.easymacher.utils.Types.OrThrowable
 
+// TODO провести рефакторинг: выделить общий трейт User, от него унаследовать реализации для auth0 и cryptokey
 case class User(id: Int, username: String, email: Email, password: String, role: Role,
                 isActivated: Boolean, activateToken: String,
-                firstName: Option[String] = None, lastName: Option[String] = None)
+                firstName: Option[String] = None, lastName: Option[String] = None,
+                auth0Id: String = "")
