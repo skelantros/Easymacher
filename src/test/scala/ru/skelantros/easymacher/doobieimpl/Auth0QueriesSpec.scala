bd3da1c454588e64f87ffbf3061cbcd6334b2811
skelantros <ad3fful@yandex.ru>
2021-12-18 13:30:18 +0300

    Removed Auth0 queries separate object

--- src/test/scala/ru/skelantros/easymacher/doobieimpl/Auth0QueriesSpec.scala
+++
@@ -1,9 +0,0 @@
-package ru.skelantros.easymacher.doobieimpl
-
-import user.Auth0Queries._
-
-class Auth0QueriesSpec extends AnalyzeSpec {
-  test("findAuth0") { check(findByAuth0Sub("")) }
-  test("addAuth0") { check(addByAuth0Sub("", false))}
-  test("addByAuth0Info") { check(addByAuth0Info("1", "", Some(""), None))}
-}
