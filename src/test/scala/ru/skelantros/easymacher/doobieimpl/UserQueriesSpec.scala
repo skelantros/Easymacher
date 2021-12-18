23ea45421291de920eee0bbec6bdb24c18946d57
skelantros <ad3fful@yandex.ru>
2021-12-18 13:25:09 +0300

    Removed tests of old queries

--- src/test/scala/ru/skelantros/easymacher/doobieimpl/UserQueriesSpec.scala
+++ src/test/scala/ru/skelantros/easymacher/doobieimpl/UserQueriesSpec.scala
@@ -12,15 +12,11 @@ class UserQueriesSpec extends AnalyzeSpec {
   test("selectAll") { check(selectAll) }
   test("selectByAdmin") { check(selectByAdmin(true)) }
   test("selectById") { check(selectById(1)) }
-  test("selectByUsername") { check(selectByUsername("someone")) }
-  test("selectByEmail") { check(selectByEmail("email")) }
-  test("selectByToken") { check(selectByToken("token")) }
-  test("update 0") { check(update(1, Some("email"), Some("username"), Some("firstName"), Some("lastName"))) }
+  test("update 0") { check(update(1, Some("username"), Some("firstName"), Some("lastName"))) }
   // test("update 1") { check(update(1, None, None, None, None)) }
-  test("update 2") { check(update(1, Some("email"), None, None, None)) }
-  test("update 3") { check(update(1, None, Some("username"), None, None)) }
-  test("update 4") { check(update(1, None, None, Some("firstName"), None)) }
-  test("update 5") { check(update(1, None, None, None, Some("lastName"))) }
-  test("create") { check(create("username", "password", "email", true, "token")) }
-  test("activate") { check(activate("token")) }
+  test("update 2") { check(update(1, None, None, None)) }
+  test("update 3") { check(update(1, Some("username"), None, None)) }
+  test("update 4") { check(update(1, None, Some("firstName"), None)) }
+  test("update 5") { check(update(1, None, None, Some("lastName"))) }
+  test("create") { check(create("auth0sub", "username", Some("firstName"), None, false)) }
 }
