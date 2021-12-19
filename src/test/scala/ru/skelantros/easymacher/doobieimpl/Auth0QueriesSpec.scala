78790dbcfbebbd74414f5fe662f6249483185626
skelantros <ad3fful@yandex.ru>
2021-12-19 18:33:57 +0300

    Delete Auth0QueriesSpec.scala

--- src/test/scala/ru/skelantros/easymacher/doobieimpl/Auth0QueriesSpec.scala
+++
@@ -1,6 +0,0 @@
-package ru.skelantros.easymacher.doobieimpl
-
-class Auth0QueriesSpec extends AnalyzeSpec {
-  test("findAuth0") { check(findByAuth0Sub("")) }
-  test("addAuth0") { check(addByAuth0Sub("", false))}
-}
