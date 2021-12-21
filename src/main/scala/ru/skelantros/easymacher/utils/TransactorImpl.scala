a940d736df3d675bbfed64dc2c830b34ccb4b9ca
skelantros <ad3fful@yandex.ru>
2021-12-21 19:29:30 +0300

    Update TransactorImpl.scala

--- src/main/scala/ru/skelantros/easymacher/utils/TransactorImpl.scala
+++ src/main/scala/ru/skelantros/easymacher/utils/TransactorImpl.scala
@@ -7,10 +7,11 @@ object TransactorImpl {
   private def dbName = System.getenv("EASYMACHER_DB_NAME")
   private def dbUser = System.getenv("EASYMACHER_DB_USER")
   private def dbPassword = System.getenv("EASYMACHER_DB_PASSWORD")
+  private def dbAddress = System.getenv("DATABASE_URL")
 
   def apply[F[_] : Async]: Transactor[F] = Transactor.fromDriverManager[F](
     "org.postgresql.Driver",
-    s"jdbc:postgresql:$dbName",
+    s"jdbc:postgresql:$dbAddress",
     dbUser,
     dbPassword
   )
